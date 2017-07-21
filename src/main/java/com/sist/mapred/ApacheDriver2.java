31a37000dc7788b021ddb9c5115ac80a6fd0a072
Unknown <shaqbari@gmail.com>
2017-07-21 11:14:51 +0900

    맵리듀스 스프링이용안하고

--- src/main/java/com/sist/mapred/ApacheDriver2.java
+++ src/main/java/com/sist/mapred/ApacheDriver2.java
@@ -8,32 +8,26 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-
-
-public class ApacheDriver {
+public class ApacheDriver2 {
 	public static void main(String[] args) {
 		try {
 			Configuration conf=new Configuration();
-			Job job=Job.getInstance(conf, "apache-log");
+			Job job=Job.getInstance(conf,"apache-log");
 			job.setMapperClass(ApacheMapper.class);
 			job.setReducerClass(ApacheReducer.class);
 			job.setJarByClass(ApacheDriver.class);
 			
-			//결과값 
+			//결과값
 			job.setOutputKeyClass(Text.class);
 			job.setOutputValueClass(IntWritable.class);
 			
-			//파일 올리기
+			//파일올리기
 			FileInputFormat.addInputPath(job, new Path("/home/sist/access_log"));
 			FileOutputFormat.setOutputPath(job, new Path("./output"));
-			
+			//결과값출력
 			job.waitForCompletion(true);
-			
-			
-			//실행
-			
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 	}
 }
--- src/main/java/com/sist/mapred/ApacheReducer.java
+++ src/main/java/com/sist/mapred/ApacheReducer.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class ApacheReduecer extends Reducer<Text, IntWritable, Text, IntWritable>{
+public class ApacheReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
 	private IntWritable res=new IntWritable();
 	
 	
